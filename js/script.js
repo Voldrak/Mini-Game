e44348bedfd3908b8c64bdf14f52a51da68a3ce6
MarcoM94 <92711603+Voldrak@users.noreply.github.com>
2021-11-20 19:56:17 +0100

    ancora da migliorare tante cose

---
+++ js/script.js
@@ -0,0 +1,195 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+const canvas = document.querySelector(".gameCanvas");
+var ctx = canvas.getContext('2d');
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+//Creazione del personaggio
+
+//Funzioni che determinano posizione x,y,raggio e colore
+
+//Player
+class Player {
+    constructor( x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
+
+
+//Proiettili
+class Projectile{
+    constructor(x,y, radius, color, velocity) {
+
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+//Nemici
+class Enemy{
+    constructor(x,y, radius, color, velocity) {
+
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+//Iniziamo a dare i valori base al player
+const x = canvas.width /2
+const y = canvas.height /2
+
+
+const player = new Player(x, y, 30, 'blue')
+const projectiles = []
+const enemies = []
+
+
+//SPAWN NEMICI
+function spawnEnmies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4
+
+    let x
+    let y
+
+    if(Math.random() < 0.5){
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        
+        }
+
+        const color = 'green'
+        
+        const angle = Math.atan2(
+        canvas.height / 2 - y,
+        canvas.width / 2 - x
+         )
+    const velocity = {
+        x: Math.cos(angle) ,
+        y: Math.sin(angle)
+    }
+
+        enemies.push(new Enemy(x,y, radius, color, velocity))
+        
+    },1000)
+}
+
+let animationId
+function animate() {
+    animationId = requestAnimationFrame(animate)
+    ctx.clearRect(0, 0,canvas.width, canvas.height)
+    player.draw()
+    projectiles.forEach((projectile, index) => {
+       projectile.update() 
+
+       // Remove from edges of screen
+       if(
+            projectile.x + projectile.radius < 0 || 
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height
+            ) {
+        setTimeout(() => {
+            projectiles.splice(index, 1)
+        }, 0)
+       }
+    })
+
+    enemies.forEach((enemy, index) => {
+        enemy.update()
+
+        const dist = Math.hypot(player.x - enemy.x,
+            player.y - enemy.y)
+// END GAME
+        if(dist - enemy.radius - player.radius < 1 ) {
+            cancelAnimationFrame(animationId)
+        }
+
+        projectiles.forEach((projectile, projectileIndex) => {
+           const dist = Math.hypot(projectile.x - enemy.x,
+                 projectile.y - enemy.y)
+
+                 //Oggetto toccato
+            if(dist - enemy.radius - projectile.radius < 1 ) {
+                setTimeout(() => {
+                    enemies.splice(index, 1)
+                    projectiles.splice(projectileIndex, 1)
+                }, 0)
+                
+
+            }
+        });
+    })
+}
+
+//Aziona proiettile
+window.addEventListener('click', (event) => {
+    console.log(projectiles);
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+         event.clientX - canvas.width / 2
+         )
+    const velocity = {
+        x: Math.cos(angle) ,
+        y: Math.sin(angle)
+    }
+
+projectiles.push(new Projectile(
+    canvas.width / 2,
+    canvas.height / 2, 5, 'red', velocity)
+    
+     )
+})
+
+
+animate()
+spawnEnmies()
+})
